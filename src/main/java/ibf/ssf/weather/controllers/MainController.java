76719d8b1e1cd5612e1c67a87a8716f31f93d7b9
Chan Yew Yen <chan.yewyen@gmail.com>
2022-01-16 11:09:24 +0800

    Fix dynamic title for landing page

--- src/main/java/ibf/ssf/weather/controllers/MainController.java
+++ src/main/java/ibf/ssf/weather/controllers/MainController.java
@@ -15,6 +15,8 @@ public class MainController {
 
     @GetMapping
     public String showForm(Model model) {
+        // populate this for the form to show dynamic title without city not found error value at landing page
+        model.addAttribute("landing", true);
         return "form";
     }
 }
